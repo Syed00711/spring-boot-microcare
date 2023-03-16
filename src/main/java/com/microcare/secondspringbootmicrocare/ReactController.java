8e1df78cd4396b8947085a8784296093eb204d36
syed Hussain <syed.hussain@cerner.com>
2023-03-16 09:59:41 +0530

    upload

--- src/main/java/com/microcare/secondspringbootmicrocare/ReactController.java
+++ src/main/java/com/microcare/secondspringbootmicrocare/ReactController.java
@@ -1,9 +1,11 @@
 package com.microcare.secondspringbootmicrocare;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ReactController {
@@ -61,5 +66,22 @@ public class ReactController {
 	 		return message;
 
 	 	 }
+	 
+	 @PostMapping(value="/reactuploadfile",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+	 public String fileUpload(@RequestParam int employee_id,@RequestPart MultipartFile file_content) throws IOException {
+		System.out.println("employeeid:"+employee_id);
+		 System.out.println("filename:"+file_content.getOriginalFilename());
+		 Employee_CV cv =new Employee_CV();	
+		 cv.setEmployee_id(employee_id);
+		cv.setFileName(file_content.getOriginalFilename());
+		cv.setContent_type(file_content.getContentType());
+		cv.setFile_size(file_content.getSize());
+		cv.setFile_content(file_content);
+	 db.insertEmployeeCV(cv);
+	 
+	
+	  return "success";
+	  
+	 }
 
 }
